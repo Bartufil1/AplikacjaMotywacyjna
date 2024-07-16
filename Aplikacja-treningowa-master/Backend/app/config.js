efbe223fe31950ad9a972b238902172b2555db67
Bartufil1 <104689537+Bartufil1@users.noreply.github.com>
2024-07-16 23:09:38 +0200

    Update config.js

--- Aplikacja-treningowa-master/Backend/app/config.js
+++ Aplikacja-treningowa-master/Backend/app/config.js
@@ -2,12 +2,11 @@ const config = {
   port: process.env.PORT || 3000,
   databaseUrl:
     process.env.MONGODB_URI ||
-    "mongodb+srv://Bartek:inzynierka2023@inzynierka.pnka94j.mongodb.net/?retryWrites=true&w=majority",
   JwtSecret: process.env.JWT_SECRET || "Secret",
-  SMTP_HOST: "smtp.gmail.com",
-  SMTP_PORT: 465,
-  SMTP_USERNAME: "bartekinformatyka1407@gmail.com",
-  SMTP_PASSWORD: "nbif xkrg myku kpvw",
+  SMTP_HOST: ,
+  SMTP_PORT: ,
+  SMTP_USERNAME: ",
+  SMTP_PASSWORD: ,
 };
 
 export default config;
